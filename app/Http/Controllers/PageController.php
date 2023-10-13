cacddbcd4188169726432c6c9e299bb08eefd253
filippo199600 <pippotartaglia@gmail.com>
2023-10-13 15:38:29 +0200

    add cards

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('homepage');
+        $movies = Movie::all();
+
+        return view('homepage', compact('movies'));
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,5 +3,20 @@
 @section('title-page', 'homepage')
 
 @section('main-content')
-    <h1>homepage</h1>
+    <div class="container">
+        @forelse ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                    <p class="card-text">{{ $movie->nationality }}</p>
+                    <p class="card-text">{{ $movie->date }}</p>
+                    <p class="card-text">{{ $movie->vote }}</p>
+                    <a href="#" class="card-link">Card link</a>
+                </div>
+            </div>
+        @empty
+            <h1>nessun dato</h1>
+        @endforelse
+    </div>
 @endsection
